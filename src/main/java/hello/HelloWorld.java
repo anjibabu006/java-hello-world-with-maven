72ff33d40e2ccf52b2807cf217ee68d7ef166cad
Janga Anjibabu <60913005+anjibabu006@users.noreply.github.com>
2026-06-18 09:50:30 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,13 +1,29 @@
 package hello;
 
 import org.joda.time.LocalTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController // Enables this class to handle web browser URLs
 public class HelloWorld {
-    public static void main(String[] args) {
-      LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is: " + currentTime);
 
+    public static void main(String[] args) {
+        // Starts the embedded web server on port 8080
+
+SpringApplication.run
+
+(HelloWorld.class, args);
+    }
+
+    @GetMapping("/") // Maps this method to http://localhost:8080/
+    public String sayHelloWeb() {
+        LocalTime currentTime = new LocalTime();
         Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        
+        // This exact string will appear on your browser screen
+        return "The current local time is: " + currentTime + "<br>" + greeter.sayHello();
     }
 }
